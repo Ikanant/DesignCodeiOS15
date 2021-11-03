fcc3f99664727c29b00ad99b53694a45300faca9
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2021-11-03 17:53:33 -0400

    Initial Commit

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by Jonathan Hernandez on 11/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            Text("Photography")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(
+                    .linearGradient(
+                        colors: [.primary, .primary.opacity(0.5)],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+            
+            Text("1 Formula available".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            
+            Text("Formulas to take your photography sklls from zero to hero!")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundStyle(.secondary)
+        }
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(
+            .ultraThinMaterial
+        )
+        .cornerRadius(30.0)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 10, y: 10)
+        .padding(.horizontal, 20)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -90)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11 Pro Max")
+            .previewInterfaceOrientation(.portrait)
+            ContentView()
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+                .previewDevice("iPhone 11 Pro Max")
+                .previewInterfaceOrientation(.portrait)
+        }
+    }
+}
