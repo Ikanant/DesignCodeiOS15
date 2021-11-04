bfea82e4dfe87613b6fc1d95a2903db521018bb7
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2021-11-03 23:11:11 -0400

    Refactor components in Account page to exist in their own VIEWs. Now the main body of the View only has a list of the name of the extracted VIEWS

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -8,72 +8,99 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "camera")
-                        .symbolVariant(.circle.fill) // This is platform specific and can change in iOS vs ipadOS vs macOS
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette) // This will allow us to tweak colors across icon layers
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background (Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image (systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                        )
-                    Text("Jonathan Hernandez").font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location").imageScale(.small) // This will not work for the Label component
-                        Text("Raleigh, NC").foregroundColor(.secondary)
-                    }
-                }
-                .frame (maxWidth: .infinity)
-                .padding()
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label ("Settings", systemImage: "gear")
-                    }
-                    // Now there is a new way to do the same thing as NavigationLink but a bit cleaner...
-                    // Check link bellow: ACTION and LABEL
-                    NavigationLink { ContentView() } label:  {
-                        Label ("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink { Text("BANANA") } label: {
-                        Label ("Help", systemImage: "questionmark")
-                    }
-                }
-                // Modifiers for the parent component will apply to all the child ones:
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
-                
-                Section {
-                    Link(destination: URL(string: "https://capturedzeal.com")!) {
-                        HStack {
-                            Label("Website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link").foregroundColor(.secondary)
-                        }
-                    }
-                    Link(destination: URL(string: "https://instagram.com/capturedzeal")!) {
-                        HStack {
-                            Label("Website", systemImage: "camera")
-                            Spacer()
-                            Image(systemName: "link").foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparator(.hidden)
+                profile
+                menu
+                links
             }
         }
         .listStyle(.sidebar)
         .navigationTitle("Account:")
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "camera")
+                .symbolVariant(.circle.fill) // This is platform specific and can change in iOS vs ipadOS vs macOS
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette) // This will allow us to tweak colors across icon layers
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background (Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image (systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+                )
+            Text("Jonathan Hernandez").font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location").imageScale(.small) // This will not work for the Label component
+                Text("Raleigh, NC").foregroundColor(.secondary)
+            }
+        }
+        .frame (maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label ("Settings", systemImage: "gear")
+            }
+            // Now there is a new way to do the same thing as NavigationLink but a bit cleaner...
+            // Check link bellow: ACTION and LABEL
+            NavigationLink { ContentView() } label:  {
+                Label ("Billing", systemImage: "creditcard")
+            }
+            NavigationLink { Text("BANANA") } label: {
+                Label ("Help", systemImage: "questionmark")
+            }
+        }
+        // Modifiers for the parent component will apply to all the child ones:
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://capturedzeal.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link").foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions (edge: .leading, allowsFullSwipe: true){
+                    Button(action: { isDeleted = true }) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red) // Will override text and will color entire icon
+                    // We can have multiple buttons:
+                    Button {} label: {
+                        Label("Pin", systemImage: "pin")
+                    }
+                    .tint(.yellow)
+                }
+            }
+            
+            Link(destination: URL(string: "https://instagram.com/capturedzeal")!) {
+                HStack {
+                    Label("Website", systemImage: "camera")
+                    Spacer()
+                    Image(systemName: "link").foregroundColor(.secondary)
+                }
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparator(.hidden)
     }
 }
 
